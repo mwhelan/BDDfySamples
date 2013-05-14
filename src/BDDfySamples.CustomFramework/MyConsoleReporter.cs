91f1d74bde86f2c0ab2c7e21c2cc6ce88e5b91f6
Michael Whelan <mjmwdev@gmail.com>
2013-05-14 03:04:59 +0100

    renamed files

--- src/BDDfySamples.CustomFramework/MyConsoleReporter.cs
+++ src/BDDfySamples.CustomFramework/MyConsoleReporter.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using TestStack.BDDfy.Configuration;
 using TestStack.BDDfy.Core;
 using TestStack.BDDfy.Processors;
 
